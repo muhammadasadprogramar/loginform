ad6afa5923a1ff1998a1cb03917ba35504875a11
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-05-28 15:55:07 +0500

    javascript login form

---
+++ app.js
@@ -0,0 +1,14 @@
+let userEmail = document.getElementById("email").value;
+let userPassword = document.getElementById("password").value;
+
+let login = document.getElementById("btn");
+
+login.addEventListener("click", () => {
+  let userEmail = document.getElementById("email").value;
+  let userPassword = document.getElementById("password").value;
+  if (userEmail == "asad9988@gmail.com" && userPassword == 112234) {
+    window.location.href = "https://www.facebook.com/";
+  } else {
+    alert("Please try again");
+  }
+});
