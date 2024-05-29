921e150f477bd163c3d45bf9e766f9c955dcc130
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-05-29 11:43:23 +0500

    link change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ login.addEventListener("click", () => {
   let userEmail = document.getElementById("email").value;
   let userPassword = document.getElementById("password").value;
   if (userEmail == "asad9988@gmail.com" && userPassword == 112234) {
-    window.location.href = "https://www.facebook.com/";
+    window.location.href = "https://github.com/muhammadasadprogramar";
   } else {
     alert("Please try again");
   }
